398907ac72805b65f94516b18f8d093ec335156e
Francois Morales <f.moraless@uniandes.edu.co>
2023-10-01 23:20:34 -0500

    controller consumos

--- src/main/java/uniandes/edu/co/proyecto/controller/PlatosConsumidosController.java
+++ src/main/java/uniandes/edu/co/proyecto/controller/PlatosConsumidosController.java
@@ -6,6 +6,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import uniandes.edu.co.proyecto.Modelo.Consumo;
+import uniandes.edu.co.proyecto.Modelo.ConsumoBebidas;
+import uniandes.edu.co.proyecto.Modelo.ConsumoBebidasPK;
+import uniandes.edu.co.proyecto.Modelo.PlatosConsumidos;
+import uniandes.edu.co.proyecto.Modelo.PlatosConsumidosPK;
+import uniandes.edu.co.proyecto.repositorio.ConsumoRepository;
+import uniandes.edu.co.proyecto.repositorio.PlatoRepository;
 import uniandes.edu.co.proyecto.repositorio.PlatosConsumidosRepository;
 
 
@@ -14,18 +22,39 @@ public class PlatosConsumidosController {
     @Autowired
     private PlatosConsumidosRepository platosConsumidosRepository;
 
-    @GetMapping("/platosConsumidos")
-    public String platosConsumidos(Model model){
-        model.addAttribute("platosConsumidos", platosConsumidosRepository.darPlatosConsumidos());
-        return "platosConsumidos";
-    }
+    @Autowired
+    private ConsumoRepository consumoRepository;
+
+    @Autowired
+    private PlatoRepository platoRepository;
+
+    // @GetMapping("/platosConsumidos")
+    // public String platosConsumidos(Model model){
+    //     model.addAttribute("platosConsumidos", platosConsumidosRepository.darPlatosConsumidos());
+    //     return "platosConsumidos";
+    // }
+
+    // @GetMapping("/platosConsumidos/new")
+    // public String platosConsumidosForm(Model model){
+    //     model.addAttribute("platosConsumidos", new uniandes.edu.co.proyecto.Modelo.PlatosConsumidos());
+    //     return "platosConsumidosNuevo";       
+    // }
 
     @GetMapping("/platosConsumidos/new")
     public String platosConsumidosForm(Model model){
         model.addAttribute("platosConsumidos", new uniandes.edu.co.proyecto.Modelo.PlatosConsumidos());
+        model.addAttribute("consumos", consumoRepository.darConsumos());
+        model.addAttribute("platos", platoRepository.darPlatos());
         return "platosConsumidosNuevo";       
     }
 
-
+    @PostMapping("/platosConsumidos/new/save")
+    public String platosConsumidosGuardar(@ModelAttribute("idConsumo")Integer idConsumo, @ModelAttribute("nombrePlato") String nombrePlato){
+        PlatosConsumidos platosConsumidos = new PlatosConsumidos();
+        PlatosConsumidosPK pk = new PlatosConsumidosPK(consumoRepository.darConsumo(idConsumo), platoRepository.darPlato(nombrePlato));
+        platosConsumidos.setPk(pk);
+        platosConsumidosRepository.insertarPlatoConsumido(platosConsumidos.getPk().getConsumos_IdConsumo().getIdConsumo(), platosConsumidos.getPk().getPlatos_nombre().getNombre());
+        return "redirect:/platosConsumidos";
+    }
 
 }
